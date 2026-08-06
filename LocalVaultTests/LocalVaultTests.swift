0588696f847d54b8ec810c559c64dcfe6e8a061d
Barba2k2 <lorenzo.dz@hotmail.com>
2026-08-06 20:26:46 -0300

    feat: add authenticated vault encryption

--- LocalVaultTests/LocalVaultTests.swift
+++ LocalVaultTests/LocalVaultTests.swift
@@ -35,4 +35,64 @@ struct LocalVaultTests {
 
     #expect(decoded == credential)
   }
+
+  @Test func cryptoServiceRoundTripsData() throws {
+    let service = CryptoKitVaultCryptoService()
+    let key = Data(repeating: 0x11, count: 32)
+    let plaintext = Data("local secret".utf8)
+
+    let ciphertext = try service.encrypt(plaintext, using: key)
+    let decrypted = try service.decrypt(ciphertext, using: key)
+
+    #expect(ciphertext != plaintext)
+    #expect(decrypted == plaintext)
+  }
+
+  @Test func cryptoServiceRejectsTamperedCiphertext() throws {
+    let service = CryptoKitVaultCryptoService()
+    let key = Data(repeating: 0x22, count: 32)
+    var ciphertext = try service.encrypt(Data("secret".utf8), using: key)
+    ciphertext[ciphertext.index(before: ciphertext.endIndex)] ^= 0x01
+
+    var failedAuthentication = false
+    do {
+      _ = try service.decrypt(ciphertext, using: key)
+    } catch VaultError.authenticationFailed {
+      failedAuthentication = true
+    }
+
+    #expect(failedAuthentication)
+  }
+
+  @Test func cryptoServiceRejectsWrongKey() throws {
+    let service = CryptoKitVaultCryptoService()
+    let key = Data(repeating: 0x33, count: 32)
+    let wrongKey = Data(repeating: 0x44, count: 32)
+    let ciphertext = try service.encrypt(Data("secret".utf8), using: key)
+
+    var failedAuthentication = false
+    do {
+      _ = try service.decrypt(ciphertext, using: wrongKey)
+    } catch VaultError.authenticationFailed {
+      failedAuthentication = true
+    }
+
+    #expect(failedAuthentication)
+  }
+
+  @Test func cryptoServiceRejectsInvalidKeyLength() {
+    let service = CryptoKitVaultCryptoService()
+    let invalidKey = Data(repeating: 0x55, count: 16)
+
+    var rejectedKey = false
+    do {
+      _ = try service.encrypt(Data("secret".utf8), using: invalidKey)
+    } catch VaultError.invalidKeyLength {
+      rejectedKey = true
+    } catch {
+      rejectedKey = false
+    }
+
+    #expect(rejectedKey)
+  }
 }
